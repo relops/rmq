9dbcc9f1a60625f23f1bcb991c0717f31ef6e612
Ben Hood <0x6e6562@gmail.com>
2014-03-13 13:37:00 +0000

    Honor the persistent flag

--- work/sender.go
+++ work/sender.go
@@ -47,7 +47,7 @@ func StartSender(signal chan error, flake *gosnow.SnowFlake, opts *Options) {
 			signal <- err
 		}
 
-		sum, err := s.send(group, opts.Exchange, opts.Key, buf)
+		sum, err := s.send(group, opts, buf)
 		if err != nil {
 			signal <- err
 		}
@@ -64,7 +64,7 @@ func StartSender(signal chan error, flake *gosnow.SnowFlake, opts *Options) {
 	signal <- nil
 }
 
-func (s *client) send(group uint64, x, key string, payload []byte) ([]byte, error) {
+func (s *client) send(group uint64, o *Options, payload []byte) ([]byte, error) {
 
 	id, err := s.flake.Next()
 	if err != nil {
@@ -81,11 +81,15 @@ func (s *client) send(group uint64, x, key string, payload []byte) ([]byte, erro
 		Headers:       amqp.Table{timestampHeader: time.Now().UnixNano()},
 	}
 
+	if o.Persistent {
+		envelope.DeliveryMode = amqp.Persistent
+	}
+
 	mandatory := false
 	immediate := false
 
-	if err := s.ch.Publish(x, key, mandatory, immediate, envelope); err != nil {
-		return nil, fmt.Errorf("Could not publish to exchange %s: %s", x, err)
+	if err := s.ch.Publish(o.Exchange, o.Key, mandatory, immediate, envelope); err != nil {
+		return nil, fmt.Errorf("Could not publish to exchange %s: %s", o.Exchange, err)
 	}
 
 	h := murmur3.New32()
